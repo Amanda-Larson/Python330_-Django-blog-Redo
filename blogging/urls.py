0ff03806f4d195e985d802cf447b06fd0961a4fe
Amanda <larson.amandarae@gmail.com>
2022-09-15 13:49:22 -0700

    url change

--- blogging/urls.py
+++ blogging/urls.py
@@ -4,6 +4,6 @@ from blogging.views import PostListView, PostDetailView
 urlpatterns = [
     # path('', list_view, name="blog_index"),
     path('', PostListView.as_view(), name="post_index"),
-    path('posts/<int:pk>', PostDetailView.as_view(), name="blog_detail"),  # <int:post_id> captures digits as the post_id
+    path('posts/<int:pk>/', PostDetailView.as_view(), name="blog_detail"),  # <int:post_id> captures digits as the post_id
 
 ]
