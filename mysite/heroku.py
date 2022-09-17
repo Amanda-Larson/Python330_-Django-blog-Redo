b492a552d8b3024a739c1b785ac63f88099b6ae7
Amanda <larson.amandarae@gmail.com>
2022-09-16 22:15:22 -0700

    fix heroku typo

--- mysite/heroku.py
+++ mysite/heroku.py
@@ -2,7 +2,7 @@ import os
 import dj_database_url
 from .settings import *
 
-DATABASE = {
+DATABASES = {
     "default": dj_database_url.config(
         default="sqlite:///" + os.path.join(BASE_DIR, "db.sqlite3")
     )
